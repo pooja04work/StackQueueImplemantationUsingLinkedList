ebb0d680ae0b2f47030c3f0f1eae3d87f30b57c7
Pooja <pooja.14206@gmail.com>
2021-05-02 19:59:31 +0530

    added a file that create a Stack using linkedlist

--- src/main/java/com/bridgelabz/Stack.java
+++ src/main/java/com/bridgelabz/Stack.java
@@ -1,5 +1,18 @@
 package com.bridgelabz;
 
 public class Stack {
+    LinkedList linkedlist = new LinkedList();
+
+    public <T> void push(MyNode<T> newNode) {
+        linkedlist.add(newNode);
+    }
+
+    public void printStack() {
+        linkedlist.printMyNode();
+    }
+
+    public INode peak() {
+        return linkedlist.head;
+    }
 
 }
--- src/test/java/com/bridgelabz/StackTest.java
+++ src/test/java/com/bridgelabz/StackTest.java
@@ -1,7 +1,20 @@
 package com.bridgelabz;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class StackTest<T> {
-
+    @Test
+    public void given3NumbersWhenAddedToStackShouldHaveLastAddedNode() {
+        MyNode<T> myFirstNode = new MyNode(70);
+        MyNode<T> mySecondNode = new MyNode(30);
+        MyNode<T> myThirdNode = new MyNode(56);
+        Stack stack = new Stack();
+        stack.push(myFirstNode);
+        stack.push(mySecondNode);
+        stack.push(myThirdNode);
+        stack.printStack();
+        INode peak = stack.peak();
+        Assert.assertEquals(myThirdNode, peak);
+    }
 }
